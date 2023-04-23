ccf392da6f4779eaa7c8d3fdbb647732ddcc3c50
moyangzhan <83471894+moyangzhan@users.noreply.github.com>
2023-04-23 13:26:14 +0800

    增加获取验证码的接口

--- src/main/java/com/ramostear/captcha/HappyCaptcha.java
+++ src/main/java/com/ramostear/captcha/HappyCaptcha.java
@@ -32,65 +32,82 @@ public class HappyCaptcha {
     private HttpServletRequest request;
     private HttpServletResponse response;
 
-    public static Builder require(HttpServletRequest request,HttpServletResponse response){
-        return new Builder(request,response);
+    private AbstractCaptcha captcha;
+
+    public static Builder require(HttpServletRequest request, HttpServletResponse response) {
+        return new Builder(request, response);
     }
 
-    public boolean finish() {
+    public HappyCaptcha finish() {
         try {
             boolean flag = false;
-            if(this.style.equals(CaptchaStyle.IMG)){
-                Captcha captcha = new Captcha();
+            if (this.style.equals(CaptchaStyle.IMG)) {
+                captcha = new Captcha();
                 captcha.setType(this.type);
                 captcha.setWidth(this.width);
                 captcha.setHeight(this.height);
                 captcha.setLength(this.length);
                 captcha.setFont(this.font);
                 setHeader(this.response);
-                this.request.getSession().setAttribute(SESSION_KEY,captcha.getCaptchaCode());
-                captcha.render(this.response.getOutputStream());
-                return true;
-            }else if(this.style.equals(CaptchaStyle.ANIM)){
-                AnimCaptcha captcha = new AnimCaptcha();
+                this.request.getSession().setAttribute(SESSION_KEY, captcha.getCaptchaCode());
+                return this;
+            } else if (this.style.equals(CaptchaStyle.ANIM)) {
+                captcha = new AnimCaptcha();
                 captcha.setType(this.type);
                 captcha.setWidth(this.width);
                 captcha.setHeight(this.height);
                 captcha.setLength(this.length);
                 captcha.setFont(this.font);
                 setHeader(this.response);
-                this.request.getSession().setAttribute(SESSION_KEY,captcha.getCaptchaCode());
-                captcha.render(this.response.getOutputStream());
-                return true;
-            }else{
-                return false;
+                this.request.getSession().setAttribute(SESSION_KEY, captcha.getCaptchaCode());
+                return this;
+            } else {
+                throw new RuntimeException("生成验证码失败");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            return false;
+            throw new RuntimeException("生成验证码失败", e);
         }
+    }
 
+    public String getCode() {
+        if (null == captcha) {
+            throw new RuntimeException("captcha is null");
+        }
+        return captcha.getCode();
+    }
+
+    public void output() {
+        try {
+            captcha.render(this.response.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("验证码输出到服务端失败", e);
+        }
     }
 
     /**
      * 校验验证码
-     * @param request           HttpServletRequest
-     * @param code              captcha code
-     * @param ignoreCase        Ignore Case
-     * @return                  boolean
+     *
+     * @param request    HttpServletRequest
+     * @param code       captcha code
+     * @param ignoreCase Ignore Case
+     * @return boolean
      */
-    public static boolean verification(HttpServletRequest request,String code,boolean ignoreCase){
-        if(code == null || code.trim().equals("")){
+    public static boolean verification(HttpServletRequest request, String code, boolean ignoreCase) {
+        if (code == null || code.trim().equals("")) {
             return false;
         }
-        String captcha = (String)request.getSession().getAttribute(SESSION_KEY);
-        return ignoreCase?code.equalsIgnoreCase(captcha):code.equals(captcha);
+        String captcha = (String) request.getSession().getAttribute(SESSION_KEY);
+        return ignoreCase ? code.equalsIgnoreCase(captcha) : code.equals(captcha);
     }
 
     /**
      * remove captcha from session
-     * @param request   HttpServletRequest
+     *
+     * @param request HttpServletRequest
      */
-    public static void remove(HttpServletRequest request){
+    public static void remove(HttpServletRequest request) {
         request.getSession().removeAttribute(SESSION_KEY);
     }
 
@@ -102,7 +119,7 @@ public class HappyCaptcha {
     }
 
 
-    private HappyCaptcha(Builder builder){
+    private HappyCaptcha(Builder builder) {
         this.type = builder.type;
         this.style = builder.style;
         this.font = builder.font;
@@ -113,7 +130,7 @@ public class HappyCaptcha {
         this.response = builder.response;
     }
 
-    public static class Builder{
+    public static class Builder {
         private CaptchaType type = CaptchaType.DEFAULT;
         private CaptchaStyle style = CaptchaStyle.IMG;
         private Font font = Fonts.getInstance().defaultFont();
@@ -123,40 +140,41 @@ public class HappyCaptcha {
         private final HttpServletRequest request;
         private final HttpServletResponse response;
 
-        public Builder(HttpServletRequest request,HttpServletResponse response){
+        public Builder(HttpServletRequest request, HttpServletResponse response) {
             this.request = request;
             this.response = response;
         }
 
-        public HappyCaptcha build(){
+        public HappyCaptcha build() {
             return new HappyCaptcha(this);
         }
 
-        public Builder type(CaptchaType type){
+        public Builder type(CaptchaType type) {
             this.type = type;
             return this;
         }
 
-        public Builder style(CaptchaStyle style){
+        public Builder style(CaptchaStyle style) {
             this.style = style;
             return this;
         }
 
-        public Builder width(int width){
+        public Builder width(int width) {
             this.width = width;
             return this;
         }
 
-        public Builder height(int height){
+        public Builder height(int height) {
             this.height = height;
             return this;
         }
 
-        public Builder length(int length){
+        public Builder length(int length) {
             this.length = length;
             return this;
         }
-        public Builder font(Font font){
+
+        public Builder font(Font font) {
             this.font = font;
             return this;
         }
